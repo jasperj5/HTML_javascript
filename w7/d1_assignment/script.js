af225d9d454620329c1e277ce2e99fe843a0f58f
jasperj5 <jnjafferian@email.neit.edu>
2020-12-10 10:28:35 -0500

    update done

---
+++ w7/d1_assignment/script.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById("myCanvas");
+var ctx =canvas.getContext("2d");
+var drawSquare;
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 150, 75)
+
+function draw(){
+    ctx.fill = "yellow"
+    ctx.drawSquare = "85, 305"
+    ctx.stroke = "black 5px wide"
+}
+
